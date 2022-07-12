2f04d1f9ea1061bfc3207d761b75721dbad5a67b
Prateek883 <singhprateek089@gmail.com>
2022-07-12 20:12:18 +0530

    Metadata changes

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,6 +1,6 @@
 const siteMetadata = {
   title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  author: 'Prateek Singh',
   headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
@@ -10,12 +10,12 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
+  email: 'singh95prateek@gmail.com',
+  github: 'https://github.com/prateek883',
+  twitter: 'https://twitter.com/prateek85165206',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/in/prateek-singh-6ab984145/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
